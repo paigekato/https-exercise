c10785c8f0144d0a239f1c94a800db8f02d2cb0f
Paige <paigekato@gmail.com>
2017-03-13 23:07:44 +0000

    uppercase & lowercae

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,11 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function printUpperCase(html) {
+  console.log(html.toLowerCase());
+}
+getHTML(requestOptions, printUpperCase);
