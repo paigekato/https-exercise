6226805260b8c9e90d8530e41286fab14e335df2
Paige <paigekato@gmail.com>
2017-03-13 18:29:12 +0000

    Adding stream

---
+++ printing.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+https.get(requestOptions, function getAndPrintHTMLChunks(response) {
+
+  response.setEncoding("utf8");
+
+  response.on("data", function(data) {
+    console.log("[--DATA OF LENGTH " + data.length + "--]" + "\n");
+    console.log(data.toString());
+  });
+  response.on("end", function() {
+    console.log("Response stream complete.");
+  });
+});
+
+
+
+
+
