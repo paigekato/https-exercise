99d8ce81e6582cebfef3d3d42e87d2f7b4bd68df
Paige <paigekato@gmail.com>
2017-03-13 23:31:29 +0000

    added password

--- callbacks/print1337.js
+++ callbacks/print1337.js
@@ -1,8 +1,27 @@
-var getHTML = require('./http-functions');
+var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 };
+function password(html) {
+  var output = "";
 
-getHTML(requestOptions);
+  for (var i = 0; i < html.length; i++) {
+    var letter = html[i];
+    if(letter == "a") { //4
+      letter = 4;
+    } else if (letter == "e") { //3
+      letter = 3;
+    } else if (letter == "o") { //0
+      letter = 0;
+    } else if (letter == "l") { //1
+      letter = 1;
+    }
+    output += letter;
+  }
+  console.log(output);
+
+}
+
+getHTML(requestOptions, password);
--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,8 +1,11 @@
-var getHTML = require('./http-functions');
+var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 };
 
-getHTML(requestOptions);
+function reverso(html) {
+  console.log(html.split("").reverse().join(""));
+}
+getHTML(requestOptions, reverso);
